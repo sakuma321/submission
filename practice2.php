204c2ec06f91812012e8e7a0be2b253cf79ac710
YUka <bibidi-babidi-boo.321@hotmail.co.jp>
2020-03-01 06:57:28 +0000

    practice

---
+++ practice2.php
@@ -0,0 +1,19 @@
+<?php
+
+$name = "Yuka";
+
+if ($name == "Yuka"){
+    echo"私はあなたの名前です";
+}
+else{
+    echo"あなたの名前ではありません。";
+    
+}
+
+$total =0;
+for($i = 0; $i<=10000 ; $i++){
+    $total +=$i;
+    
+}
+
+echo $total;
